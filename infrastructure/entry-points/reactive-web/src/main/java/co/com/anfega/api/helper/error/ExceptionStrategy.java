5256c25eb5cb75c794527fa18534ca3298bcba71
Andres Felipe Gañan Moreno <andres.ganan@pragma.com.co>
2025-09-24 12:21:48 -0500

    ADD: se agrega libreria common

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/anfega/api/helper/error/ExceptionStrategy.java
+++
@@ -1,9 +0,0 @@
-package co.com.anfega.api.helper.error;
-
-import org.springframework.http.HttpStatus;
-
-public interface ExceptionStrategy {
-    boolean supports(Throwable ex);
-    HttpStatus getStatus(Throwable ex);
-    String getError(Throwable ex);
-}
